d91cb21b5346e97e40ad86089113705dcdf3fdb5
Asou Gima <e235708@ie.u-ryukyu.ac.jp>
2024-01-30 23:44:45 +0900

    3rd

---
+++ src/Report5_3.java
@@ -0,0 +1,9 @@
+public class Report5_3 {
+    public static void main(String[] args) {
+        String str = "壱百満";
+
+        int value = Integer.parseInt(str);
+
+        System.out.println(value);
+    }
+}
