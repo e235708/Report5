4f01d9646148e206949880fba002368dcaf698a7
Asou Gima <e235708@ie.u-ryukyu.ac.jp>
2024-01-30 22:30:05 +0900

    2nd

--- src/Report5_1.java
+++ src/Report5_1.java
@@ -1,7 +1,13 @@
 public class Report5_1 {
     public static void main(String[] args){
-        String str = null;
+        try{
 
-        System.out.println(str.length());
+            String str = null;
+
+            System.out.println(str.length());
+        } catch(NullPointerException e) { 
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
